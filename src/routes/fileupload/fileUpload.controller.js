54167b916ca3ab40415666055708a42b628220df
PankEcal <pankaj.ecal@gmail.com>
2023-04-17 16:18:23 +0530

    fileUpload: clean up controller file

--- src/routes/fileupload/fileUpload.controller.js
+++ src/routes/fileupload/fileUpload.controller.js
@@ -23,33 +23,6 @@ function isValidReqData(request) {
 	}
 }
 
-// make POST request with uploaded file
-async function makeHttpReq(uploadUrl) {
-	try {
-		const response = await axios.get(uploadUrl);
-		const {
-			headers: { date },
-			request: {
-				method,
-				res: { statusCode, statusMessage },
-			},
-			data,
-		} = response;
-
-		let responseData = {
-			statusCode: statusCode,
-			statusMessage: statusMessage,
-			method: method,
-			date: date,
-			data: data,
-		};
-
-		return responseData;
-	} catch (error) {
-		console.error(error);
-	}
-}
-
 // make http POST request with uploaded file
 async function makePostReq(uploadUrl, csvfile, deviceId) {
 	try {
@@ -99,6 +72,7 @@ async function makePostReq(uploadUrl, csvfile, deviceId) {
 	}
 }
 
+// get device ID from the csv file
 function getDeviceId() {
 	const deviceIdFilePath = path.join(
 		__dirname,
