24fb74b51851a7f2f513e0947ef36135f0e8c9d3
PankEcal <pankaj.ecal@gmail.com>
2023-04-20 12:26:12 +0530

    implement CORS access for the app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const fileUploadRouter = require("./routes/fileupload/fileUpload.router");
 const dailyMoniterRouter = require("./routes/dailymonitor/dailymonitor.router");
 
+let corsOptions = {
+	origin: "*",
+};
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 /* Implementing routes */
