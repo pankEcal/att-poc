d8a59313ee9f219bfdffdade9dfba34564ab162f
PankEcal <pankaj.ecal@gmail.com>
2023-04-26 16:42:50 +0530

    udpdate codebase with comments for readability

--- src/routes/staticrequests/staticrequests.controller.js
+++ src/routes/staticrequests/staticrequests.controller.js
@@ -20,12 +20,15 @@ async function clearFiles() {
 	});
 }
 
-function validateParams(validationParams, serverResponses) {
+// method to reutrn validation message based on the values passed
+function validate(validationParams, serverResponses) {
+	// creating validation message object and a map to store results
 	const validationMessage = {};
 	const resultsMap = new Map();
 	resultsMap.set("passed", 0);
 	resultsMap.set("failed", 0);
 
+	// check if validation params are present or not, if not then it will be handled inside the block, and won't proceed further
 	if (!validationParams) {
 		Object.assign(validationMessage, {
 			validationMessage: "validation check skipped. No values passed.",
@@ -34,19 +37,25 @@ function validateParams(validationParams, serverResponses) {
 		return validationMessage;
 	}
 
+	// iterate through each validation param and check if it is present in server response
 	for (key in validationParams) {
+		// if the validation param isn't present in server response then consider it failed case
 		if (String(serverResponses[key]) === "undefined") {
 			resultsMap.set("failed", resultsMap.get("failed") + 1);
 		} else {
+			// defining a validation condition. If value of the validation param and server param are strictly matching then it's validated and is passing case.
 			const isMatchingValidationParam =
 				String(validationParams[key]) === String(serverResponses[key]);
 
+			// update validation result map based on condition
 			isMatchingValidationParam
 				? resultsMap.set("passed", resultsMap.get("passed") + 1)
 				: resultsMap.set("failed", resultsMap.get("failed") + 1);
 		}
 	}
 
+	// check in validation message for the passing case and update the result map accordingly.
+	// passing condition: at least 1 passing case should be there and no failed case should be present.
 	if (resultsMap.get("passed") >= 1 && resultsMap.get("failed") === 0) {
 		Object.assign(validationMessage, {
 			validationMessage:
@@ -59,17 +68,22 @@ function validateParams(validationParams, serverResponses) {
 		});
 	}
 
+	// return the validation based on the conditions checked
 	return validationMessage;
 }
 
+// method to make POST request to the requested server, and send back the data to main function
 async function makeHttpReq(req) {
+	// check if request body is empty, if it's empty then don't proceed further
 	if (!Boolean(Object.keys(req.body).length)) {
-		const data = { status: "false", message: "missing required input data" };
+		const data = { status: false, message: "missing required input data" };
 
 		return { data, status: 400 };
 	}
 
+	// use try catch block to handle POST request to the provided server
 	try {
+		// if file is uploaded from client side, then it will be handled in this block
 		if (req.file) {
 			const {
 				body: { baseUrl, apiLink, ...requestParams },
@@ -96,32 +110,45 @@ async function makeHttpReq(req) {
 			return { data, status };
 		}
 
+		// get required data from request body to make request
 		const {
 			body: { baseUrl, apiLink, requestParams, validationParams },
 		} = req;
 
+		// check if baseUrl and apiLink fields are empty, In that case, it will be handled inside this block and won't proceed further
 		if (!baseUrl || !apiLink) {
 			return {
 				status: false,
-				message: "URL input invalid or incomplete",
+				message: "URL inputs invalid or incomplete",
 			};
 		}
 
+		// if  baseUrl and apiLink fields are non-empty then make POST request with the provided request body
 		const serverResponse = await axios.post(baseUrl + apiLink, requestParams, {
 			headers: { "Content-Type": "application/json" },
 		});
+
+		// get server response after making POST requst to the provided URL
 		const { data, status } = serverResponse;
+		// get validation message
+		const validationMessage = validate(validationParams, data);
 
-		const validationMessage = validateParams(validationParams, data);
-
+		// return server response data, validation message, and server responded statusCode
 		return { data: { ...data, ...validationMessage }, status };
 	} catch (error) {
-		const data = { status: "false", message: error.message };
+		// if error is occured then pass the message and status codes accordingly
+		const data = { status: false, message: error.message };
 		return { ...data, status: 400 };
 	}
 }
 
+/* ============================================ */
+/* ====== main methods to handle requests ===== */
+/* ============================================ */
+
+// method to make POST request to server and give response back
 async function httpGeStaticRequest(req, res) {
+	// collecting response coming from the requested server side, and sending respose back to client side
 	const { data, status } = await makeHttpReq(req);
 
 	return res.status(status).json({
