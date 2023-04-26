7490bd859febeac2103dd646ea3ea6ef10fefb93
PankEcal <pankaj.ecal@gmail.com>
2023-04-26 15:59:27 +0530

    implement validation test for non file upload tests

--- src/routes/staticrequests/staticrequests.controller.js
+++ src/routes/staticrequests/staticrequests.controller.js
@@ -20,6 +20,48 @@ async function clearFiles() {
 	});
 }
 
+function validateParams(validationParams, serverResponses) {
+	const validationMessage = {};
+	const resultsMap = new Map();
+	resultsMap.set("passed", 0);
+	resultsMap.set("failed", 0);
+
+	if (!validationParams) {
+		Object.assign(validationMessage, {
+			validationMessage: "validation check skipped. No values passed.",
+		});
+
+		return validationMessage;
+	}
+
+	for (key in validationParams) {
+		if (String(serverResponses[key]) === "undefined") {
+			resultsMap.set("failed", resultsMap.get("failed") + 1);
+		} else {
+			const isMatchingValidationParam =
+				String(validationParams[key]) === String(serverResponses[key]);
+
+			isMatchingValidationParam
+				? resultsMap.set("passed", resultsMap.get("passed") + 1)
+				: resultsMap.set("failed", resultsMap.get("failed") + 1);
+		}
+	}
+
+	if (resultsMap.get("passed") >= 1 && resultsMap.get("failed") === 0) {
+		Object.assign(validationMessage, {
+			validationMessage:
+				"validation check passed. User input and server response is matching",
+		});
+	} else {
+		Object.assign(validationMessage, {
+			validationMessage:
+				"validation check failed. User input and server response not matching",
+		});
+	}
+
+	return validationMessage;
+}
+
 async function makeHttpReq(req) {
 	if (!Boolean(Object.keys(req.body).length)) {
 		const data = { status: "false", message: "missing required input data" };
@@ -54,12 +96,25 @@ async function makeHttpReq(req) {
 			return { data, status };
 		}
 
+		const {
+			body: { baseUrl, apiLink, requestParams, validationParams },
+		} = req;
+
+		if (!baseUrl || !apiLink) {
+			return {
+				status: false,
+				message: "URL input invalid or incomplete",
+			};
+		}
+
 		const serverResponse = await axios.post(baseUrl + apiLink, requestParams, {
 			headers: { "Content-Type": "application/json" },
 		});
 		const { data, status } = serverResponse;
 
-		return { data, status };
+		const validationMessage = validateParams(validationParams, data);
+
+		return { data: { ...data, ...validationMessage }, status };
 	} catch (error) {
 		const data = { status: "false", message: error.message };
 		return { ...data, status: 400 };
