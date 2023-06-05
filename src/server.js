b8d80514c7b3705cb4775141f44fee22b9e714f9
PankEcal <pankaj.ecal@gmail.com>
2023-06-05 16:15:51 +0530

    update server PORT config

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const app = require("./app");
 
 const server = http.createServer(app); // create a HTTP server and host it with express app
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
 	console.log(`app is running on PORT ${PORT}`);
